0e3f5f985c704723d55df7f2ffece1f539dc0a47
Serhii H Inv <serhii.havryliuk@eliftech.com>
2024-10-10 22:37:05 +0300

    add messages for serial port and display

--- WebServerRemoteControl.ino
+++ WebServerRemoteControl.ino
@@ -1,19 +1,19 @@
 /*********
   Serhii 
+  RELEASE_VERSION = "v2024.2"
+  Serial monitor controls
 *********/
 
 // TFT Dispaly
-#include <TFT_eSPI.h> // Graphics and font library for ST7735 driver chip
+#include <TFT_eSPI.h>  // Graphics and font library for ST7735 driver chip
 #include <SPI.h>
 
 TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 
-#define TFT_GREY 0x5AEB // New colour
+#define TFT_GREY 0x5AEB  // New colour
 // TFT Dispaly
 
-const char* RELEASE_VERSION = "v2024.0";
-
-
+const char* RELEASE_VERSION = "v2024.2";
 
 // Auxiliar variables to store the current output state
 String FirePower_1 = "off";
@@ -24,13 +24,22 @@ const int pinFirePower_1 = 26;
 const int pinFireTamper_1 = 27;
 
 void setup() {
-// TFT Dispaly
+  Serial.begin(115200);
+
+  Serial.println("Start setup");
+  Serial.println(RELEASE_VERSION);
+  
+  showMainMenu("Start setup");
+  delay(1000);
+  showMainMenu(RELEASE_VERSION);
+  delay(1000);
+
+  // TFT Dispaly
   tft.init();
   tft.setRotation(1);
-  clear_display();                     // Очищення екрану
-// TFT Dispaly
+  clear_display();  // Очищення екрану
+   // TFT Dispaly
 
-  Serial.begin(115200);
   // Initialize the output variables as outputs
   pinMode(pinFirePower_1, OUTPUT);
   pinMode(pinFireTamper_1, OUTPUT);
@@ -38,15 +47,17 @@ void setup() {
   digitalWrite(pinFirePower_1, LOW);
   digitalWrite(pinFireTamper_1, LOW);
 
-  Serial.print("Setup end");
+  Serial.println("Setup end");
+  showMainMenu("Setup end");
+  delay(1000);
 }
 
 void loop() {
   // Перевірка вхідних даних із Serial Monitor
   if (Serial.available() > 0) {
-    String command = Serial.readStringUntil('\n'); // Читання команди до символу нового рядка
-    command.trim(); // Видалення пробілів на початку та в кінці команди
-    handleSerialCommand(command); // Виклик функції обробки команд
+    String command = Serial.readStringUntil('\n');  // Читання команди до символу нового рядка
+    command.trim();                                 // Видалення пробілів на початку та в кінці команди
+    handleSerialCommand(command);                   // Виклик функції обробки команд
   }
 }
 
@@ -68,23 +79,25 @@ void handleSerialCommand(String command) {
     Serial.println(RELEASE_VERSION);
   } else {
     Serial.println("Unknown command: " + command);
+    showMainMenu("Unknown command: " + command);
   }
+  showMainMenu(command);
 }
 
 // -------------------------------------------------------------------------------
-// Показати головне меню 
+// Показати головне меню
 // todo: потрібно на екран виводити стан системи, дебаг та помилки
 // -------------------------------------------------------------------------------
-void showMainMenu(String text){
+void showMainMenu(String text) {
   tft.fillScreen(TFT_GREY);
   tft.setCursor(0, 4, 4);  // Встановлюємо курсор для виводу тексту
   tft.println("Menu:");    // Виводимо заголовок меню
   tft.println();
 
   tft.setTextColor(TFT_WHITE);  // Білий колір для тексту
-  tft.print("URL ");
-  tft.setTextColor(TFT_YELLOW);         // Жовтий колір для температури
-  tft.println(text);  // Виводимо початкову температуру
+  tft.print("State: ");
+  tft.setTextColor(TFT_YELLOW);  // Жовтий колір для температури
+  tft.println(text);             // Виводимо початкову температуру
 }
 
 // -------------------------------------------------------------------------------
