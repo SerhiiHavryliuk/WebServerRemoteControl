70ccb9aab17153ae4168b1ab54600314e79d47a5
Serhii H Inv <serhii.havryliuk@eliftech.com>
2024-10-14 20:18:33 +0300

    add comand for 2 and 3 debices

--- README.md
+++ README.md
@@ -8,6 +8,7 @@
    ####  http://192.168.1.106/27/off
 
 ## Comands Serial Monitor
+
 ### 1 Chanel
   ### Запит
     #### request_ch_1_power_on - вкл кнопку power на датчику
@@ -20,8 +21,32 @@
     #### response_ch_1_tamper_on - вкл кнопку tamper на датчику
     #### response_ch_1_tamper_off - викл кнопку tamper на датчику
 
-  ### Системні запити
-    #### release_version - повертає версію прошивки
+### 2 Chanel
+  ### Запит
+    #### request_ch_2_power_on - вкл кнопку power на датчику
+    #### request_ch_2_power_off - викл кнопку power на датчику
+    #### request_ch_2_tamper_on - вкл кнопку tamper на датчику
+    #### request_ch_2_tamper_off - викл кнопку tamper на датчику
+  ### Відповідь на запит, якщо запит був успішний
+    #### response_ch_2_power_on - вкл кнопку power на датчику
+    #### response_ch_2_power_off - викл кнопку power на датчику
+    #### response_ch_2_tamper_on - вкл кнопку tamper на датчику
+    #### response_ch_2_tamper_off - викл кнопку tamper на датчику
+
+### 3 Chanel
+  ### Запит
+    #### request_ch_3_power_on - вкл кнопку power на датчику
+    #### request_ch_3_power_off - викл кнопку power на датчику
+    #### request_ch_3_tamper_on - вкл кнопку tamper на датчику
+    #### request_ch_3_tamper_off - викл кнопку tamper на датчику
+  ### Відповідь на запит, якщо запит був успішний
+    #### response_ch_3_power_on - вкл кнопку power на датчику
+    #### response_ch_3_power_off - викл кнопку power на датчику
+    #### response_ch_3_tamper_on - вкл кнопку tamper на датчику
+    #### response_ch_3_tamper_off - викл кнопку tamper на датчику
+
+### Системні запити
+  #### release_version - повертає версію прошивки
 
 ## User interface
 ![UI Interface Screenshot](./Screenshot-ui-interface.png)
--- WebServerRemoteControl.ino
+++ WebServerRemoteControl.ino
@@ -13,22 +13,27 @@ TFT_eSPI tft = TFT_eSPI();  // Invoke library, pins defined in User_Setup.h
 #define TFT_GREY 0x5AEB  // New colour
 // TFT Dispaly
 
-const char* RELEASE_VERSION = "v2024.2";
-
-// Auxiliar variables to store the current output state
-String FirePower_1 = "off";
-String FireTamper_1 = "off";
+const char* RELEASE_VERSION = "v2024.3";
 
 // Assign output variables to GPIO pins
+// Device 1
 const int pinDevicePower_1 = 13;
 const int pinDeviceTamper_1 = 12;
+
+// Device 2
+const int pinDevicePower_2 = 27;
+const int pinDeviceTamper_2 = 26;
+
+// Device 3
+const int pinDevicePower_3 = 33;
+const int pinDeviceTamper_3 = 32;
 
 void setup() {
   Serial.begin(115200);
 
   Serial.println("Start setup");
   Serial.println(RELEASE_VERSION);
-  
+
   showMainMenu("Start setup");
   delay(1000);
   showMainMenu(RELEASE_VERSION);
@@ -38,14 +43,29 @@ void setup() {
   tft.init();
   tft.setRotation(1);
   clear_display();  // Очищення екрану
-   // TFT Dispaly
+                    // TFT Dispaly
 
   // Initialize the output variables as outputs
+  // Device 1
   pinMode(pinDevicePower_1, OUTPUT);
   pinMode(pinDeviceTamper_1, OUTPUT);
+  // Device 2
+  pinMode(pinDevicePower_2, OUTPUT);
+  pinMode(pinDeviceTamper_2, OUTPUT);
+  // Device 3
+  pinMode(pinDevicePower_3, OUTPUT);
+  pinMode(pinDeviceTamper_3, OUTPUT);
+
   // Set outputs to LOW
+  // Device 1
   digitalWrite(pinDevicePower_1, LOW);
   digitalWrite(pinDeviceTamper_1, LOW);
+  // Device 2
+  digitalWrite(pinDevicePower_2, LOW);
+  digitalWrite(pinDeviceTamper_2, LOW);
+  // Device 3
+  digitalWrite(pinDevicePower_3, LOW);
+  digitalWrite(pinDeviceTamper_3, LOW);
 
   Serial.println("Setup end");
   showMainMenu("Setup end");
@@ -75,6 +95,30 @@ void handleSerialCommand(String command) {
   } else if (command == "request_ch_1_tamper_off") {
     digitalWrite(pinDeviceTamper_1, LOW);
     Serial.println("response_ch_1_tamper_off");
+  } else if (command == "request_ch_2_power_on") {
+    digitalWrite(pinDevicePower_2, HIGH);
+    Serial.println("response_ch_2_power_on");
+  } else if (command == "request_ch_2_power_off") {
+    digitalWrite(pinDevicePower_2, LOW);
+    Serial.println("response_ch_2_power_off");
+  } else if (command == "request_ch_2_tamper_on") {
+    digitalWrite(pinDeviceTamper_2, HIGH);
+    Serial.println("response_ch_2_tamper_on");
+  } else if (command == "request_ch_2_tamper_off") {
+    digitalWrite(pinDeviceTamper_2, LOW);
+    Serial.println("response_ch_2_tamper_off");
+  } else if (command == "request_ch_3_power_on") {
+    digitalWrite(pinDevicePower_3, HIGH);
+    Serial.println("response_ch_3_power_on");
+  } else if (command == "request_ch_3_power_off") {
+    digitalWrite(pinDevicePower_3, LOW);
+    Serial.println("response_ch_3_power_off");
+  } else if (command == "request_ch_3_tamper_on") {
+    digitalWrite(pinDeviceTamper_3, HIGH);
+    Serial.println("response_ch_3_tamper_on");
+  } else if (command == "request_ch_3_tamper_off") {
+    digitalWrite(pinDeviceTamper_3, LOW);
+    Serial.println("response_ch_3_tamper_off");
   } else if (command == "release_version") {
     Serial.println(RELEASE_VERSION);
   } else {
